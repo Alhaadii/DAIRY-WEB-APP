0e14168f3dbc7126eb34c53d9e3f68da629df9b8
Alhaadii <alcazaam1233@gmail.com>
2025-01-21 09:21:47 +0300

    Dairy-webapp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="text-uppercase mt-5 text-center">
+        welcome to the export default
+      </h1>
+    </div>
+  );
+};
+
+export default App;
