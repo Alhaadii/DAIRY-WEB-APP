9ef96305cf1ef0e5b67d6f29a576b5c7163b1bd7
Alhaadii <alcazaam1233@gmail.com>
2025-01-21 10:08:26 +0300

    update data virtually and localstorage

---
+++ frontend/src/utils/Data.jsx
@@ -0,0 +1,24 @@
+export const events = [
+  {
+    _id: "1",
+    title: "Event 1",
+    description: "Description of event 1",
+    eventDate: new Date(Date.now()),
+    user: {
+      _id: "1",
+      name: "Sulieman Mohamed Abdirahman",
+      email: "hiamdwise@gmail.com",
+    },
+  },
+  {
+    _id: "2",
+    title: "Event 2",
+    description: "Description of event 2",
+    eventDate: new Date(Date.now()),
+    user: {
+      _id: "2",
+      name: "ahmed Mohamed Abdirahman",
+      email: "zarqaawi@gmail.com",
+    },
+  },
+];
